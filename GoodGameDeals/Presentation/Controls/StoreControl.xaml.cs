93d8c969a52530286328ed15b7249382405f679d
Unknown <drdgvhbh@gmail.com>
2018-01-05 13:10:40 -0500

    add buttons

---
+++ GoodGameDeals/Presentation/Controls/StoreControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace GoodGameDeals.Presentation.Controls {
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    public sealed partial class StoreControl : UserControl {
+        public static readonly DependencyProperty StoreProperty =
+            DependencyProperty.Register(
+                "Store",
+                typeof(string),
+                typeof(StoreControl),
+                new PropertyMetadata("Store"));
+
+        public StoreControl() {
+            this.InitializeComponent();
+        }
+
+        public string Store {
+            get => (string)this.GetValue(StoreProperty);
+
+            set => this.SetValue(StoreProperty, value);
+        }
+    }
+}
